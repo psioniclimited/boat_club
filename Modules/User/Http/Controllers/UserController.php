c691e6fd763ce10f9eed73ac5bd10e759a0f54f0
arafatarshad <arafatarshad@amadeyr.org>
2017-04-10 16:08:40 +0600

    Create user & create_user_process created

--- Modules/User/Http/Controllers/UserController.php
+++ Modules/User/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 use Form;
 use Auth;
+use Modules\User\Entities\Role;
 class UserController extends Controller
 {
     /**
@@ -18,21 +19,38 @@ class UserController extends Controller
         return view('user::login');
     }
 
-    public function loginUser(Request $request){ 
-       $credentials=array(
-            'email' => $request->email, 
-            'password' => $request->password
-        );  
-        if (Auth::attempt($credentials)) { 
+    public function loginUser(Request $request){
+        $credentials=array( 'email' => $request->email,  'password' => $request->password);
+        if (Auth::attempt($credentials)) {
             return redirect()->intended('/');
-        } 
+        }
         return back()->withInput();
     }
 
-public function createUsers() {
-    $getRoles = Role::all();
-    return view('user::create_users',['getRoles'=>$getRoles]);
-}
+    public function createUsers() {
+        $getRoles = Role::all(); 
+        return view('user::create_users',['getRoles'=>$getRoles]);
+    }
+
+    public function createUsersProcess(Request $request) {  
+         dd($request->all());
+        $addUsers = new User();
+
+        $addUsers->name = $request->input('fullname');
+        $addUsers->email = $request->input('uemail');
+        $addUsers->password = bcrypt($request->input('upassword'));
+
+        $addUsers->save();
+
+        $userID = $addUsers->id;
+        $roleID = $request->input('uroles');
+
+        $user = User::find($userID);
+        $role = Role::where('id', '=', $roleID)->get()->first();
+        $user->attachRole($role);
+
+        return redirect('allusers');
+    }
 
     /**
      * Show the form for creating a new resource.
--- Modules/User/Http/routes.php
+++ Modules/User/Http/routes.php
@@ -3,5 +3,7 @@
 Route::group(['middleware' => 'web', 'prefix' => 'user', 'namespace' => 'Modules\User\Http\Controllers'], function()
 {
     Route::get('/login', 'UserController@index');
-    Route::post('/login', 'UserController@loginUser');
+    Route::post('/login', 'UserController@loginUtser');
+    Route::get('/create_users', 'UserController@createUsers');
+    Route::post('/create_users_process', 'UserController@createUsersProcess');
 });
